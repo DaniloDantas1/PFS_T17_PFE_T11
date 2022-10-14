e11f3a78a0a0ee32db66075bbd360acd9976590b
Danilo Dantas <danilogdantas2@gmail.com>
2022-10-14 11:15:00 -0300

    vs code er2

---
+++ UC8/ER2/exemplo01.js
@@ -0,0 +1,38 @@
+/*regras para operadores de comparação
+= (é utilizado para atribuição)
+== para comparação (verifica se é igual)
+=== para comparação (verifica se é identico ou n)
+!= diferente
+> maior
+< menor
+<= menor ou igual
+>= maior ou igual
+*/
+
+
+/* Operadores de atribuição
+=atribuição
++= (x+=y) é a mesma coisa que (x= x + y)
+-= (x-=y) é a mesma coisa que (x= x - y)
+*= (x*=y) é a mesma coisa que (x= x * y)
+/= (x/=y) é a mesma coisa que (x= x / y)
+*/
+
+
+/* Operadores lógicos
+&& (e)
+|| (ou)
+! (negação)
+*/
+
+let x = true
+let y = true
+let resultado = (x!y)
+consolelog (resultado)
+
+if(!variavel){
+    
+}
+else{
+
+}
